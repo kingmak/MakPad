5a7219cb25e237fe809211b34cf6976bd95c2721
kingmak <kingmak@hotmail.com>
2016-07-31 21:13:47 -0500

    body select all works, full screen launch True
    select all for the body works now
    full screen launch now works

--- MakPad.py
+++ MakPad.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+
+'''
+TODO: fix root quit
+TODO: self resizing widgets, self place widges
+'''
+
 import os, sys, time, tkFont, urllib2, linecache, tkFileDialog, tkMessageBox
 from Tkinter import *
 
@@ -17,9 +23,10 @@ def EntrySelectAll(event, ObjectName):
     ObjectName.select_range (0, end)
     return 'break'
 
-def BodySelectAll(event, ObjectName):
-    end = len(ObjectName.get(1.0, END)[:-1])
-    #ObjectName.select_range (0, end)
+def BodySelectAll(event):
+    Body.tag_add(SEL, "1.0", END)
+    Body.mark_set(INSERT, "1.0")
+    Body.see(INSERT)
     return 'break'
 
 def EntryPopupMenu(event, Determiner):
@@ -29,12 +36,12 @@ def EntryPopupMenu(event, Determiner):
     FileMenu.add_command(label = 'Paste', command = lambda: Determiner.event_generate('<<Paste>>'))
     FileMenu.add_command(label = 'Undo', command = lambda: Determiner.event_generate('<<Undo>>'))
     FileMenu.post(event.x_root, event.y_root)
-
+    
 def MainMenus():
     FileName = ''    
     def OpenFile():
         Count = 1
-        File = str(tkFileDialog.askopenfilename(initialdir = os.getcwd(), title= 'Browse to a File to Open'))#if File == '': error
+        File = str(tkFileDialog.askopenfilename(initialdir = os.getcwd(), title = 'Browse to a File to Open'))#if File == '': error
         if File != '':
             FileName = File
             InserFileName = FileName.split('/')
@@ -59,7 +66,8 @@ def MainMenus():
         elif '.' not in DocName.get():
             Name = DocName.get() + '.makn'            
         else:
-            Name = DocName.get()  
+            Name = DocName.get()
+            
         FilePlace = str(tkFileDialog.askdirectory(initialdir = os.getcwd(), title = 'Where do u wanna save this File?'))
         File = open(os.path.join(FilePlace, Name), 'w')
         File.write(Body.get(1.0, END)[:-1])
@@ -69,6 +77,10 @@ def MainMenus():
         filewin = Toplevel(root)
         button = Button(filewin, text = 'Still Working on This One :D')
         button.pack()
+
+    def quitRoot():
+        window = Toplevel(root)
+        window.destroy()
           
     Menubar = Menu(root)
     Filemenu = Menu(Menubar, tearoff = 0)
@@ -86,10 +98,8 @@ def MainMenus():
     Editmenu.add_separator()
     Editmenu.add_command(label = 'Undo', command = lambda: Body.event_generate('<<Undo>>'))
     Editmenu.add_command(label = 'Delete', command = donothing)
-    Editmenu.add_command(label = 'Select All', command = lambda: Body.event_generate('<<SelectAll>>'))
-    #Editmenu.add_command(label = 'Select All', command = donothing)
     Editmenu.add_separator()
-    Editmenu.add_command(label = 'Exit', command = root.quit)
+    Editmenu.add_command(label = 'Exit', command = quitRoot)
     root.config(menu = Menubar)
 
 root = Tk()
@@ -99,8 +109,8 @@ root.option_add('*Background', 'grey30')#grey30
 root.configure(bg = 'grey30')#grey63
 MainMenus()
 
-w, h = (root.winfo_screenwidth() - 20), (root.winfo_screenheight() - 118)
-root.geometry('%dx%d+%d+%d' % (w, h, 0, 0))
+w, h = (root.winfo_screenwidth()), (root.winfo_screenheight())
+root.geometry("{0}x{1}+0+0".format(w, h - 118))
 
 DocNameLab = Label(root, width = 14, fg = 'orange', text = 'Document Name:', anchor = 'w').place(x = 15, y = 10)
 DocName = Entry(root, relief = RIDGE, width = 50, fg = 'orange', selectbackground = 'black')
@@ -109,17 +119,22 @@ DocName.bind('<Control-Key-A>', lambda event: EntrySelectAll(event, DocName))
 DocName.bind('<Button-3>', lambda event: EntryPopupMenu(event, DocName))
 DocName.place(x = 230, y = 12)
 
-TimeStampInsert = Button(root, fg = 'orange', text = 'Insert Time Stamp', command = InsertTimeStamp).place(x = 950, y = 4)
-CommentInsert = Button(root, fg = 'orange', text = 'Insert Comment Lines', command = InsertUserBio).place(x = 1215, y = 4)
+TimeStampInsert = Button(root, fg = 'orange', text = 'Insert Time Stamp', command = InsertTimeStamp).place(x = 800, y = 4)
+CommentInsert = Button(root, fg = 'orange', text = 'Insert Comment Lines', command = InsertUserBio).place(x = 1014, y = 4)
 
 RightScrollbar = Scrollbar(root)
 RightScrollbar.pack(side = RIGHT, fill = Y)
 
-Body = Text(root, width = 132, height = 33, fg = 'orange', selectbackground = 'white', highlightcolor = 'red', wrap = WORD, undo = True, yscrollcommand = RightScrollbar.set)
+Body = Text(root, width = 112, height = 25, fg = 'orange', selectbackground = 'white', highlightcolor = 'red', wrap = WORD, undo = True, yscrollcommand = RightScrollbar.set)
 Body.bind('<Button-3>', lambda event: EntryPopupMenu(event, Body))
-Body.bind('<Control-Key-a>', lambda: Body.event_generate('<<SelectAll>>'))
+Body.bind('<Control-Key-a>', lambda event: BodySelectAll(event))
+Body.bind('<Control-Key-A>', lambda event: BodySelectAll(event))
 RightScrollbar.config(command = Body.yview)
 Body.place(x = 14, y = 55)
 
 root.title('MakPad')
+root.state('zoomed') # launch full screen
 root.mainloop()
+
+
+
